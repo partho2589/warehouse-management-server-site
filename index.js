ffab122e68b3777152ab5983cf47fe1bf2c75623
partho2589 <parthomalo615@.com>
2022-05-06 13:42:05 +0600

    added mongodb client site

--- index.js
+++ index.js
@@ -9,6 +9,32 @@ const port = process.env.POST || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0tju7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('success your canection ')
+
+async function run () {
+    try {
+        await client.connect()
+        const productCollection = client.db('warehouse').collection('product')
+       
+        app.get('/product' , (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const product = await  cursor.toArray();
+            res.send(product)
+        })
+
+    } catch (error) {
+        
+    }
+}
+run()
+
+
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
